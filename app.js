7e1974d7f185aa6d1de6c2e66be06142e3e271b8
andremcguire <a.mcguire@leonardo.com.au>
2019-01-18 15:08:45 +1100

    updated

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var fs = require('fs'),
-  //https = require('https'),
+  https = require('https'),
   express = require('express'),
   app = express();
 var bodyParser = require('body-parser');
+
+
 http = require('http')
 
 //app.use(bodyParser.urlencoded());
@@ -15,12 +17,12 @@ app.use(function (req, res, next) {
   next();
 });
 
-http.createServer(app).listen(8080);
+// http.createServer(app).listen(8080);
 
-// https.createServer({
-//   key: fs.readFileSync('./sslcert/key.key'),
-//   cert: fs.readFileSync('./sslcert/certificate.crt')
-// }, app).listen(8080);
+https.createServer({
+  key: fs.readFileSync('./sslcert/key.pem'),
+  cert: fs.readFileSync('./sslcert/cert.pem')
+}, app).listen(8081);
 
 app.get('/', function (req, res) {
   res.header('Content-type', 'text/html');
@@ -33,46 +35,50 @@ app.post('/', function (req, res) {
 
   //var body = JSON.parse(req.body);
   var body = req.body;
-  var logoutput = "[" + new Date() + "]";
+  var logoutput = "[" + new Date() + "] " + JSON.stringify(req.body);
   console.log(logoutput);
-  if (body.a != undefined) {
-    logoutput = logoutput + "|Source: " + body.a;
-    console.log(logoutput);
+  // if (body.a != undefined) {
+  //   logoutput = logoutput + "|Source: " + body.a;
+  //   console.log(logoutput);
 
 
-    if (body.a == "Excel") {
+  //   if (body.a == "Excel") {
 
-      if (body.b != undefined) {
-        logoutput = logoutput + "|Type: " + body.b;
-        console.log(logoutput);
-      };
+  //     if (body.b != undefined) {
+  //       logoutput = logoutput + "|Type: " + body.b;
+  //       console.log(logoutput);
+  //     };
 
-      if (body.c != undefined) {
-        logoutput = logoutput + "|Address: " + body.c;
-        console.log(logoutput);
-      };
+  //     if (body.c != undefined) {
+  //       logoutput = logoutput + "|Address: " + body.c;
+  //       console.log(logoutput);
+  //     };
 
-      if (body.d != undefined) {
-        logoutput = logoutput + "|Values: " + body.d;
-        console.log(logoutput);
-      };
-    };
-    logoutput = logoutput + "|";
-    console.log(logoutput);
-  };
+  //     if (body.d != undefined) {
+  //       logoutput = logoutput + "|Values: " + body.d;
+  //       console.log(logoutput);
+  //     };
+  //   };
+  //   logoutput = logoutput + "|";
+  //   console.log(logoutput);
+  // }
+  // else {
+  //   logoutput = req.body;
+  // };
 
   console.log("write-to-file activated");
 
   fs.appendFile('logs.txt', JSON.stringify(logoutput) + "\n", function (err) {
     if (err) throw err;
     console.log('Append File Saved!');
-    res.write(JSON.stringify(req.body));
+    // res.write(JSON.stringify(req.body));
+    res.write("https appended");
     res.end();
   });
 
 });
 
-console.log('Server running at 8080');
+console.log('Server running at 8081');
 // const http = require('http');
 // const fs = require("fs");
 
